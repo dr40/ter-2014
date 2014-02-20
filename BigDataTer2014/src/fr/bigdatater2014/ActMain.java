fe06c3dcbb21e52f11fe0bf563bf8f479f48fb22
Jordan <jordan@truc.fr>
2014-02-20 11:57:39 +0100

    Commit map buggy

--- BigDataTer2014/src/fr/bigdatater2014/ActMain.java
+++ BigDataTer2014/src/fr/bigdatater2014/ActMain.java
@@ -1,10 +1,12 @@
 package fr.bigdatater2014;
 
+import com.google.android.maps.MapActivity;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class ActMain extends Activity {
+public class ActMain extends MapActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +14,11 @@ public class ActMain extends Activity {
         setContentView(R.layout.activity_act_main);
     }
 
-
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
