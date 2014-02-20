b037f6eadd9705b15dd81c0f4e1cb74d64fd027f
dr40 <dorian.rodde@univ-tlse3.fr>
2014-02-20 08:44:54 +0100

    First commit

---
+++ BigDataTer2014/src/fr/bigdatater2014/ActMain.java
@@ -0,0 +1,23 @@
+package fr.bigdatater2014;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ActMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.act_main, menu);
+        return true;
+    }
+    
+}
